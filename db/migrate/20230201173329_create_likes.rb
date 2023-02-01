ea15dca4683df5e4942a9061d13d300dd5842fbc
jfoyarzo <foyarzo621@gmail.com>
2023-02-01 14:38:40 -0300

    Add CreateLikes migration

---
+++ db/migrate/20230201173329_create_likes.rb
@@ -0,0 +1,11 @@
+# rubocop:disable Style/SymbolProc
+class CreateLikes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :likes do |t|
+      t.timestamps
+    end
+    add_reference :likes, :author, foreign_key: { to_table: :users }
+    add_reference :likes, :post, foreign_key: true
+  end
+end
+# rubocop:enable Style/SymbolProc
