bd42f5f2161b59070ed231dffa73cdc3e5ad835a
jfoyarzo <foyarzo621@gmail.com>
2023-02-01 23:02:57 -0300

    Add scope recent_posts to User model to retrieve the last 3 posts for a given user id

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
   has_many :likes
+  scope :recent_posts, lambda { |user_id|
+                         Post.where(author_id: user_id).last(3)
+                       }
 end
