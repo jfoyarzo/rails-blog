6851e7931731165f3b3fc8c0a4016dee53df35f4
jfoyarzo <foyarzo621@gmail.com>
2023-02-02 14:30:25 -0300

    Update associations in User model and simplify the scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
-  has_many :posts
-  has_many :comments
-  has_many :likes
-  scope :recent_posts, lambda { |user_id|
-                         Post.where(author_id: user_id).last(3)
-                       }
+  has_many :posts, foreign_key: 'author_id', inverse_of: :user
+  has_many :comments, foreign_key: 'author_id', inverse_of: :user
+  has_many :likes, foreign_key: 'author_id', inverse_of: :user
+  scope :recent_posts, ->(user_id) { Post.where(author_id: user_id).last(3) }
 end
