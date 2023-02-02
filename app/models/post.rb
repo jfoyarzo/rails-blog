1731c69eba21d290e18010a31c75caf128977e8f
jfoyarzo <foyarzo621@gmail.com>
2023-02-02 15:17:39 -0300

    Add recent_comments scope and update_post_counter class method to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,11 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
   belongs_to :user, foreign_key: 'author_id'
+  scope :recent_comments, ->(post_id) { Comment.where(post_id:).last(5) }
+
+  def self.update_post_counter(user_id)
+    user = User.find(user_id)
+    counter = user.posts.count
+    user.update(posts_counter: counter)
+  end
 end
