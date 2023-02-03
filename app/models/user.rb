2d744fe2fc97d41ff34bcd6cc74ba4e7141c07cb
jfoyarzo <foyarzo621@gmail.com>
2023-02-03 15:48:08 -0300

    Add test for recent_posts scope on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id', inverse_of: :user
   has_many :comments, foreign_key: 'author_id', inverse_of: :user
   has_many :likes, foreign_key: 'author_id', inverse_of: :user
-  scope :recent_posts, ->(user_id) { Post.where(author_id: user_id).last(3) }
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+  scope :recent_posts, ->(user_id) { Post.where(author_id: user_id).last(3) }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,13 +6,26 @@ RSpec.describe User, type: :model do
   end
   before { subject.save }
 
-  it 'should have a name to be valid' do
-    subject.name = nil
-    expect(subject).to be_invalid
+  describe 'Validations' do
+    it 'should have a name to be valid' do
+      subject.name = nil
+      expect(subject).to be_invalid
+    end
+
+    it 'should have an integer greater or equal to 0 as posts_counter to be valid' do
+      subject.posts_counter = -1
+      expect(subject).to be_invalid
+    end
   end
 
-  it 'should have an integer greater or equal to 0 as posts_counter to be valid' do
-    subject.posts_counter = -1
-    expect(subject).to be_invalid
+  describe '#recent_posts' do
+    let(:post1) { Post.create(title: 'Title1', text: 'text1', author_id: subject.id) }
+    let(:post2) { Post.create(title: 'Title2', text: 'text2', author_id: subject.id) }
+    let(:post3) { Post.create(title: 'Title3', text: 'text3', author_id: subject.id) }
+    let(:post4) { Post.create(title: 'Title4', text: 'text4', author_id: subject.id) }
+
+    it 'should return only the last 3 posts' do
+      expect(User.recent_posts(subject.id)).to eq(subject.posts.last(3))
+    end
   end
 end
