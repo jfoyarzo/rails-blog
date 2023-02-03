fa2c00bee9fd66679fdda54887145c8c04d420dc
jfoyarzo <foyarzo621@gmail.com>
2023-02-03 14:24:09 -0300

    Add validation for posts_counter

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   has_many :likes, foreign_key: 'author_id', inverse_of: :user
   scope :recent_posts, ->(user_id) { Post.where(author_id: user_id).last(3) }
   validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
 end
