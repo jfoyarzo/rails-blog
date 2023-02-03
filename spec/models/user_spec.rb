adb244f8c667fd40fd9135e3d7599e75e6ca3837
jfoyarzo <foyarzo621@gmail.com>
2023-02-02 21:21:28 -0300

    Add test to check name validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    User.new(name: 'Test User', photo: 'www.testimage.com', bio: 'Just a user generated for the purposes of testing')
+  end
+  before { subject.save }
+
+  it 'should have a name to be valid' do
+    subject.name = nil
+    expect(subject).to be_invalid
+  end
 end
