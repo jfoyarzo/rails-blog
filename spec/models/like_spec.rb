9229802ed78ac91f42109793ad071b7271e394f4
jfoyarzo <foyarzo621@gmail.com>
2023-02-03 17:32:15 -0300

    Add Like model test

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#update_likes_counter' do
+    it 'should update comments_counter on user' do
+      user = User.create(name: 'Test User', posts_counter: 0)
+      post = user.posts.build(title: 'Test Post', comments_counter: 0, likes_counter: 0)
+      post.author_id = user.id
+      like = post.likes.build(author_id: user.id)
+      like.post_id = post.id
+      post.save!
+      like.save!
+      Like.update_likes_counter(post.id)
+      expect(Post.find(post.id).likes_counter).to eq(1)
+    end
+  end
 end
