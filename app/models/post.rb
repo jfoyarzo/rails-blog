f24f0513b4cca0e13c11ea1b10ca2355bb01ea53
jfoyarzo <foyarzo621@gmail.com>
2023-02-09 18:28:53 -0300

    Update post and user models to initialize counters before validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,10 +6,18 @@ class Post < ApplicationRecord
   validates :comments_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
   scope :recent_comments, ->(post_id) { Comment.where(post_id:).last(5) }
+  before_validation :initialize_counters
 
   def self.update_posts_counter(user_id)
     user = User.find(user_id)
     counter = user.posts.count
     user.update(posts_counter: counter)
+  end
+
+  private
+
+  def initialize_counters
+    self.comments_counter ||= 0
+    self.likes_counter ||= 0
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
   scope :recent_posts, ->(user_id) { Post.where(author_id: user_id).last(3) }
+  before_validation :initialize_posts_counter
+
+  private
+
+  def initialize_posts_counter
+    self.posts_counter ||= 0
+  end
 end
