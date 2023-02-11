13f8839432e313ca1cd00040e8713f72ec7c7939
jfoyarzo <foyarzo621@gmail.com>
2023-02-11 00:31:12 -0300

    Update routes for new and create post

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'posts/new', to: 'posts#new', as: 'new_post'
+  post 'posts/new', to: 'posts#create', as: 'create_post'
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show new create] do
+    resources :posts, only: %i[index show] do
       resources :comments, only: %i[new create]
       resources :likes, only: %i[create]
     end
