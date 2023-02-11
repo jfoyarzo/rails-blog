85effa3131aa331d4bf28680ef80cde47ad1426a
jfoyarzo <foyarzo621@gmail.com>
2023-02-11 00:31:31 -0300

    Update posts controller with create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,5 +17,20 @@ class PostsController < ApplicationController
     end
   end
 
-  def create; end
+  def create
+    user = current_user
+    post = Post.new(params.require(:post).permit(:title, :text, author_id: user.id))
+    post.author_id = user.id
+    respond_to do |format|
+      format.html do
+        if post.save
+          flash[:notice] = 'Post saved successfully'
+          redirect_to users_path
+        else
+          flash.now[:error] = 'Error: Post could not be saved'
+          render :new, locals: { post: }, status: 422
+        end
+      end
+    end
+  end
 end
