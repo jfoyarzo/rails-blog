6c17cd20f18e25239e88b3e29529913245d9bec9
jfoyarzo <foyarzo621@gmail.com>
2023-02-11 02:27:53 -0300

    Update routes.rb with correct post routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  root 'users#index'
   get 'posts/new', to: 'posts#new', as: 'new_post'
   post 'posts/new', to: 'posts#create', as: 'create_post'
+  post 'comments/new', to: 'comments#create', as: 'create_comment'
+  post 'likes/new', to: 'likes#create', as: 'create_like'
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show] do
-      resources :comments, only: %i[new create]
-      resources :likes, only: %i[create]
-    end
+    resources :posts, only: %i[index show]
   end
 end
