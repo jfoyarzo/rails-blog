9746621061855d2e769add5c5b3bd566f3bd6fca
jfoyarzo <foyarzo621@gmail.com>
2023-02-19 20:56:25 -0300

    Add ability file

---
+++ app/models/ability.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    return unless user.present?
+
+    can(:read, :all)
+
+    can(%i[create destroy], Post, author: user)
+    can([:destroy], Comment, author: user)
+
+    return unless user.role == 'admin'
+
+    can :destroy, Post
+    can :destroy, Comment
+  end
+end
